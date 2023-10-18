3def33757e28cc8f945d77703cf6c2782bc83972
r0778068 <r0778068@student.thomasmore.be>
2023-10-18 15:13:14 +0200

    update

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,11 @@
-export default async (request, context) => {
-  return Response.json({
-    message: "hello world, it is me"
-  })
-}
+import express, { Router } from "express";
+import serverless from "serverless-http";
+
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+export const handler = serverless(api);

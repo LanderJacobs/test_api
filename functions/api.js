e698ee3305a18067f6f1fe50d5d6aa15af5d24d8
r0778068 <r0778068@student.thomasmore.be>
2023-10-18 22:52:22 +0200

    console log check 2

--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,7 @@ export default async (request, context) => {
     console.log(request)
     console.log("-----")
     console.log(context)
-    let test = JSON.parse(request.body)
+    let test = JSON.parse(request)
 
     return Response.json(
         test

e5adb6e16cef08e86a5f37b04d861b0ec7018162
r0778068 <r0778068@student.thomasmore.be>
2023-10-18 23:22:27 +0200

    cannot focus on one thing

--- functions/api.js
+++ functions/api.js
@@ -1,10 +1,20 @@
-export default async (request, context) => {
-    // log(request)
-    // log("-------")
-    // log(context)
-    // let test = JSON.parse(request.httpMethod)
-
-    return Response.json(
-        {message: JSON.stringify(context)}
-    )
-}
+exports.handler = async (event, context) => {
+    if (event.httpMethod === 'GET') {
+      try {
+        // Process the GET request as needed
+        const data = require('./db.json');
+  
+        // Return the data as the response
+        return {
+          statusCode: 200,
+          body: JSON.stringify(data),
+        };
+      } catch (error) {
+        // Return an error response if there was an issue processing the request
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ error: 'Failed to process GET request' }),
+        };
+      }
+    }
+  };

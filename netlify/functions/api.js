7463224b9d25899101a50a97a2b1b1b6715f6188
r0778068 <r0778068@student.thomasmore.be>
2023-10-18 16:40:13 +0200

    update

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,7 @@
 import express, { Router } from "express";
+require('dotenv').config();
 import serverless from "serverless-http";
+import mongoose from "mongoose";
 
 const api = express();
 
@@ -8,4 +10,5 @@ router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
 
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedToology: true});
 export const handler = serverless(api);

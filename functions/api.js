2085e949d70bb0a0a21e7fffbeb8fc642c3b45e0
r0778068 <r0778068@student.thomasmore.be>
2023-10-18 23:09:17 +0200

    one step further

--- functions/api.js
+++ functions/api.js
@@ -5,6 +5,6 @@ export default async (request, context) => {
     // let test = JSON.parse(request.httpMethod)
 
     return Response.json(
-        {message: "hello world!"}
+        {message: JSON.stringify(request.httpMethod)}
     )
 }

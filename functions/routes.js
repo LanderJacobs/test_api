69694beeeb8688e92065243cdc280b130308c86a
r0778068 <r0778068@student.thomasmore.be>
2023-10-20 14:00:33 +0200

    updated routes

--- functions/routes.js
+++ functions/routes.js
@@ -8,66 +8,67 @@ const Hats = require('./models/hats.json');
 router.get('/', (req, res) => {
     console.log("called /");
     res.send(
-        '<h1>welcome to api, shit works here</h1>'
+        '<h1>welcome to the hats-api</h1>'
         +'<br/>'
+        +'<hr/>'
         +'<h2>/reviews</h2>'
-        +'think about it'
+        +'This gets all the reviews.'
         
         +'<hr/>'
         +'<h2>/reviews/hat/:hatid</h2>'
-        +'you can find every review belonging to a specific hat'
+        +'You can find every review belonging to a specific hat.'
         
         +'<hr/>'
         +'<h2>/review/:id</h2>'
-        +"You'll find what you're looking for"
+        +"You'll find a specific review using the correct id."
         
         +'<hr/>'
         +'<h2>/review/create</h2>'
-        +'please add a body without id'
+        +'To use this one, you can add a body without id.'
         
         +'<hr/>'
         +'<h2>/review/update/:id</h2>'
         +'please add a body and the id in the link'
         +'\n'
-        +'you do not have to add everything in the body, only the things you want to change'
+        +'You do not have to add everything in the body, only the values you want to change.'
         
         +'<hr/>'
         +'<h2>/review/delete/:id</h2>'
-        +'only id is enough, then it is GONE'
+        +'Only the id is enough.'
         
         +'<hr/>'
         +'<h2>/hats</h2>'
-        +'hats');
+        +'Gets a list of the hats.');
 });
 
 //GET
 
 router.get("/reviews", async (req, res) => {
-    console.log("/reviews kunnen gelezen worden");
+    console.log("/reviews has been called");
     try {
         res.json(await Review.find());
     } catch(e) {
-        console.log(`oepsie-poepsie: ${e}`);
+        console.log(e);
         res.sendStatus(500);
     }
 });
 
 router.get("/review/:id", async (req,res) => {
-    console.log("looking specifically for a review");
+    console.log("/review/:id has been called");
     try {
         res.send(await Review.findById(req.params.id));
     } catch (e) {
-        console.log(`oh no: ${e}`);
+        console.log(e);
         res.sendStatus(500);    
     }
 });
 
 router.get("/reviews/hat/:hatid", async (req, res) => {
-    console.log("/reviews kunnen gelezen worden");
+    console.log("/reviews/hat/:hatid has been called");
     try {
         res.json(await Review.find({hatid: req.params.hatid}));
     } catch(e) {
-        console.log(`oepsie-poepsie: ${e}`);
+        console.log(e);
         res.sendStatus(500);
     }
 })
@@ -75,12 +76,12 @@ router.get("/reviews/hat/:hatid", async (req, res) => {
 //POST
 
 router.post('/review/create', async (req, res) => {
-    console.log("another one");
+    console.log("/review/create has been called");
     const data = JSON.parse(req.body);
     try{
         res.send(await Review.create(data));
     } catch (e) {
-        console.log(`why no post: ${e}`);
+        console.log(e);
         res.sendStatus(500);
     }
 });
@@ -88,12 +89,12 @@ router.post('/review/create', async (req, res) => {
 //PUT
 
 router.put('/review/update/:id', async (req, res) => {
-    console.log("time for change");
+    console.log("/review/update/:id has been called");
     const data = JSON.parse(req.body);
     try{
         res.send(await Review.findByIdAndUpdate(req.params.id, { $set: data}));
     } catch (e) {
-        console.log(`no-one wants to change: ${e}`);
+        console.log(e);
         res.sendStatus(500);
     }
 });
@@ -101,11 +102,11 @@ router.put('/review/update/:id', async (req, res) => {
 //DELETE
 
 router.delete('/review/delete/:id', async (req,res) => {
-    console.log("leave and never come back");
+    console.log("/review/delete/:id has been called");
     try {
         res.send(await Review.findByIdAndDelete(req.params.id))
     } catch (e) {
-        console.log(`You can never delete me: ${e}`);
+        console.log(e);
         res.send(500);
     }
 });
@@ -113,12 +114,12 @@ router.delete('/review/delete/:id', async (req,res) => {
 //GET hats, it is a special boy
 
 router.get('/hats', async (req, res) => {
-    console.log("there goes the hats");
+    console.log("/hats has been called");
     try{
         res.json(await Hats);
     }
     catch (e) {
-        console.log(`NOOOOOO: ${e}`);
+        console.log(e);
         res.send(500);
     }
 });

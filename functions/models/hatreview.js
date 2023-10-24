0757c213b74fe28cda981f1aff96f81326ac3e9e
r0778068 <r0778068@student.thomasmore.be>
2023-10-24 11:54:47 +0200

    updated datatypes

--- functions/models/hatreview.js
+++ functions/models/hatreview.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const hatReviewSchema = new mongoose.Schema({
-    hatid: {type: String},
+    hatid: {type: Number},
     username: {type: String},
-    rating: {type: String},
+    rating: {type: Number},
     review: {type: String}
 }, {
     collection: 'hatreview',
